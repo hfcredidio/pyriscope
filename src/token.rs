660de9f5e9e3616a3f5f29702d6462508500557b
heitor <heitor@redbaron.local>
2019-04-13 09:51:15 +0100

    Add placeholder tokens

--- src/token.rs
+++ src/token.rs
@@ -11,3 +11,33 @@ named!(pub name<Span, Node>, do_parse!(
         line: position.line,
     })
 ));
+
+named!(pub number<Span, Node>, do_parse!(
+    value: is_a!("0123456789") >>
+    position: position!() >>
+    (Node::Terminal {
+        tok: Token::Number,
+        value: value.to_string(),
+        line: position.line,
+    })
+));
+
+named!(pub string<Span, Node>, do_parse!(
+    value: recognize!(delimited!(tag!("'"), is_a!("a"), tag!("'"))) >>
+    position: position!() >>
+    (Node::Terminal {
+        tok: Token::String,
+        value: value.to_string(),
+        line: position.line,
+    })
+));
+
+named!(pub newline<Span, Node>, do_parse!(
+    value: is_a!("\n") >>
+    position: position!() >>
+    (Node::Terminal {
+        tok: Token::Newline,
+        value: value.to_string(),
+        line: position.line,
+    })
+));
